7add0d38454c0c3cb13d853d42900fe1bfec5938
hsm0512 <202653743+hsm0512@users.noreply.github.com>
2025-06-10 11:34:50 +0900

    [追加]コメント機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     belongs_to :job_category
     belongs_to :style
     attachment :image
+    has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_08_105049) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_10_020518) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "job_categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
