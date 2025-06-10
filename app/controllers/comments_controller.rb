1a30ac0dba70f0ffc588e610efe8cd83fcc54805
hsm0512 <202653743+hsm0512@users.noreply.github.com>
2025-06-10 17:52:03 +0900

    [追加]エラーメッセージ表示の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 class CommentsController < ApplicationController
     def create
         post = Post.find(params[:post_id])
-        comment = current_user.comments.new(comment_params)
-        comment.post_id = post.id
-        comment.user_id = current_user.id
-        comment.save
-        redirect_to post_path(post)
+        @comment = current_user.comments.new(comment_params)
+        @comment.post_id = post.id
+        @comment.user_id = current_user.id
+        if @comment.save
+            redirect_to post_path(post)
+        else
+            @post = Post.find(params[:post_id])
+            @comments = @post.comments.page(params[:page]).per(7)
+            render "posts/show", status: :unprocessable_entity
+        end
     end
 
     def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,11 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
 
+    validates :title, presence: true, length: { maximum: 50 }
+    validates :content, presence: true, length: { maximum: 200 }
+    validates :job_category_id, presence: true
+    validates :style_id, presence: true 
+
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
