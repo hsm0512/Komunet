cfbc6a8d05d5782c6da5c5ea1b143dc9d8532cb8
hsm0512 <202653743+hsm0512@users.noreply.github.com>
2025-06-10 12:31:03 +0900

    [追加]投稿一覧ページにユーザー情報を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  attachment :profile_image
+  attachment :profile_image, type: :image
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 end
