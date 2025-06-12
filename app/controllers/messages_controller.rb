6bce6e315f182ee315a91118ccc67cd65773ce4b
hsm0512 <202653743+hsm0512@users.noreply.github.com>
2025-06-12 16:57:45 +0900

    [修正]DM機能微修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,24 @@
 class MessagesController < ApplicationController
-    before_action :authenticate_user!, :only => [:create]
+    before_action :authenticate_user!, only: [:create]
 
-    def create
-        if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-            @message = Message.create(params.require(:message).permit(:content, :user_id, :content, :room_id).merge(:user_id => current_user.id))
-            redirect_to "/room/#{@message.room_id}"
+   def create
+    if Entry.exists?(user_id: current_user.id, room_id: params[:message][:room_id])
+        @message = Message.new(message_params)
+        @message.user_id = current_user.id
+        if @message.save
+            redirect_to room_path(@message.room_id)
         else
+            flash[:alert] = "メッセージの送信に失敗しました"
             redirect_back(fallback_location: root_path)
         end
+    else
+        redirect_back(fallback_location: root_path)
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :room_id)
+  end
 end
